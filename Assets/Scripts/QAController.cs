149320ca0e0f1de538551f05a09fada0cfdff717
Yumi <s9101505@gmail.com>
2017-12-14 16:11:50 +0800

    基本問答功能完成
    計時、純數隨機連續出題、角色及怪物血量設計、答案確認

--- Assets/Scripts/QAController.cs
+++ Assets/Scripts/QAController.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QAController : MonoBehaviour {
+	
+	float timer_f = 0f;
+	int timer = 0;
+
+	public Text timerText;
+
+	const int AnswerCountDownTime = 30;//倒數時間
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +20,13 @@ public class QAController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		/*//計時
+		timer_f += Time.deltaTime;
+		timer = (int)timer_f;
+
+		timerText.text = "時間：" + (AnswerCountDownTime-timer) + "s";*/
 		
 	}
+
 }
