269ee660343c2741a10b4086342ebad06fe84e8d
Jimmy Lin <ljhjimmy87@gmail.com>
2017-12-25 19:48:17 +0800

    change background depending on the battleground

---
+++ Assets/Scripts/BackGroundController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackGroundController : MonoBehaviour {
+
+	public Sprite grass;
+	public Sprite desert;
+	public Sprite rock;
+	public Sprite ice;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable()
+	{
+		SceneManager.activeSceneChanged += changeBackground;
+	}
+
+	void OnDisable()
+	{
+		SceneManager.activeSceneChanged -= changeBackground;
+	}
+
+	void changeBackground(Scene previousScene, Scene nextScene){
+		Debug.Log ("previousScene  " + previousScene.name);
+		switch (previousScene.name) {
+		case "battlegroundUp":
+			gameObject.GetComponent<SpriteRenderer> ().sprite = grass;
+			break;
+
+		case "battlegroundRight":
+			gameObject.GetComponent<SpriteRenderer> ().sprite = desert;
+			break;
+
+		case "battlegroundLeft":
+			gameObject.GetComponent<SpriteRenderer> ().sprite = ice;
+			break;
+
+		case "battlegroundDown":
+			gameObject.GetComponent<SpriteRenderer> ().sprite = rock;
+			break;
+
+		default:
+			gameObject.GetComponent<SpriteRenderer> ().sprite = grass;
+			break;
+		}
+	}
+}
